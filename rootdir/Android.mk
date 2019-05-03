8ed829e976a234022de4f7a831e8796fb9cfb573
SamarV-121 <samarvispute121@gmail.com>
2019-05-03 07:42:23 -0400

    mt6765-common: Fix typo

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,6 +12,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6765
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.6765
+LOCAL_SRC_FILES    := etc/fstab.mt6765
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
